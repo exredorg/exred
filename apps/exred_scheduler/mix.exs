be207d58cfaa38237a015c4f67ecf8cb94501bb9
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-03-17 02:27:14 -0400

    fixed deps in mix files

--- apps/exred_scheduler/mix.exs
+++ apps/exred_scheduler/mix.exs
@@ -26,17 +26,7 @@ defmodule Exred.Scheduler.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:exred_node_trigger,                 path: "../../node_library/exred_node_trigger"},
-      {:exred_node_debug,                   path: "../../node_library/exred_node_debug"},
-      {:exred_node_multiply,                path: "../../node_library/exred_node_multiply"},
-      {:exred_node_redis_daemon,            path: "../../node_library/exred_node_redis_daemon"},
-      {:exred_node_aws_iot_daemon,          path: "../../node_library/exred_node_aws_iot_daemon"},
-      {:exred_node_redis_in,                path: "../../node_library/exred_node_redis_in"},
-      {:exred_node_redis_out,               path: "../../node_library/exred_node_redis_out"},
-      {:exred_node_aws_iot_thingshadow_in,  path: "../../node_library/exred_node_aws_iot_thingshadow_in"},
-      
-      {:wobserver, "~> 0.1"}
-      # {:exred_library, in_umbrella: true, app: false}
+      {:exred_library, git: "git@bitbucket.org:zsolt001/exred_library.git", app: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true},
--- mix.exs
+++ mix.exs
@@ -17,7 +17,17 @@ defmodule Exred.Umbrella.Mixfile do
   # Run "mix help deps" for examples and options.
   defp deps do
       [ 
-        { :uuid, "~> 1.1" },
+        {:exred_node_trigger,                 git: "git@bitbucket.org:zsolt001/exred_node_trigger"},
+        {:exred_node_debug,                   git: "git@bitbucket.org:zsolt001/exred_node_debug"},
+        {:exred_node_multiply,                git: "git@bitbucket.org:zsolt001/exred_node_multiply"},
+        {:exred_node_redis_daemon,            git: "git@bitbucket.org:zsolt001/exred_node_redis_daemon"},
+        {:exred_node_aws_iot_daemon,          git: "git@bitbucket.org:zsolt001/exred_node_aws_iot_daemon"},
+        {:exred_node_redis_in,                git: "git@bitbucket.org:zsolt001/exred_node_redis_in"},
+        {:exred_node_redis_out,               git: "git@bitbucket.org:zsolt001/exred_node_redis_out"},
+        {:exred_node_aws_iot_thingshadow_in,  git: "git@bitbucket.org:zsolt001/exred_node_aws_iot_thingshadow_in"},
+      
+        {:wobserver, "~> 0.1"},
+        {:uuid, "~> 1.1" },
       ]
   end
 end
