b93e9153e3fa9a7d80909b2523a5b6f9fb787902
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-03-20 23:46:10 -0400

    config clean up

--- config/config.exs
+++ config/config.exs
@@ -9,12 +9,6 @@ use Mix.Config
 # back to each application for organization purposes.
 import_config "../apps/*/config/config.exs"
 
-# Sample configuration (overrides the imported configuration above):
-#
-#     config :logger, :console,
-#       level: :info,
-#       format: "$date $time [$level] $metadata$message\n",
-#       metadata: [:user_id]
 
 config :logger, :console,
   level: :debug,
