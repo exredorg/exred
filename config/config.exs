0f0ce1a2ef5d157ca4539bd186a9f9b94e9ca8b0
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-03-22 22:35:19 -0400

    changed aws certs

--- config/config.exs
+++ config/config.exs
@@ -20,6 +20,6 @@ config :logger, :console,
 # these get passed to the Erlang ssl module
 # see ssl_option() here: http://erlang.org/doc/man/ssl.html
 config :exred_node_aws_iot_daemon, :ssl,
-  keyfile: "/Users/zkeszthelyi/src/exred/certs/ff925dd2f1-private.pem.key",
-  certfile: "/Users/zkeszthelyi/src/exred/certs/ff925dd2f1-certificate.pem.crt",
+  keyfile: "/Users/zkeszthelyi/src/exred/certs/c592a5868f-private.pem.key",
+  certfile: "/Users/zkeszthelyi/src/exred/certs/c592a5868f-certificate.pem.crt",
   cacertfile: "/Users/zkeszthelyi/src/exred/certs/symantec_ca_root.pem"
