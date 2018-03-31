6e13b4aad9b176363556c228714692ad968c3f75
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-03-31 00:14:25 -0400

    ui refresh

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Exred.Umbrella.Mixfile do
 
   def project do
     [
-      version: "0.1.1",
+      version: "0.1.2",
       apps_path: "apps",
       start_permanent: Mix.env == :prod,
       deps: deps(),
--- ui/app/components/x-info-tab.js
+++ ui/app/components/x-info-tab.js
@@ -3,12 +3,4 @@ import {inject as service} from '@ember/service';
 
 export default Component.extend({
   state: service('global-state'),
-  
-  init: function(){
-    this._super(...arguments);
-    let data = {hello: 12};
-  },
-  didInsertElement: function(){
-    // this.$('.xjson').appendChild(this.get('formatter').render());
-  }
 });
