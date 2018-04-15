4375f24e359939b1ee00c9098968ebd1a7dd22e7
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-04-15 16:12:10 -0400

    git subrepo clone git@bitbucket.org:zsolt001/exred_node_redis_daemon apps/exred_node_redis_daemon
    subrepo:
      subdir:   "apps/exred_node_redis_daemon"
      merged:   "b964e00"
    upstream:
      origin:   "git@bitbucket.org:zsolt001/exred_node_redis_daemon"
      branch:   "master"
      commit:   "b964e00"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ apps/exred_node_redis_daemon/mix.exs
@@ -0,0 +1,28 @@
+defmodule Exred.Node.RedisDaemon.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :exred_node_redis_daemon,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:exred_library, git: "git@bitbucket.org:zsolt001/exred_library.git"},
+      {:redix, ">= 0.0.0"}
+    ]
+  end
+end
