d3306184a46dea5f8c47d9f3c2bcea5c247badd5
Zsolt Keszthelyi <zsolt.erl@gmail.com>
2018-06-20 21:03:46 -0400

    updated docker bundle script

--- docker/bundle_for_rpi3.sh
+++ docker/bundle_for_rpi3.sh
@@ -3,6 +3,11 @@
 # save images in a tar file
 docker save -o rpi3_images.tar exred_db_rpi3 exred_rpi3 
 
+
+tar -cvf BUNDLE.tar rpi3_images.tar env_file docker-compose_rpi3.yml rpi3_docker_load.sh -C ~/ exred_data
+
+exit 0
+
 # tar up ~/exred_data
 tar cvf rpi3_exred_data.tar -C ~/ exred_data
 
