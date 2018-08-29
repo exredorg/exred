bc3d87a7ca3615f107f4faf51d2f3680c36366c3
Zsolt Keszthelyi <zkeszthelyi@zkeszthel-ltm.internal.salesforce.com>
2018-08-29 18:17:31 -0400

    fix in RaspberryPi docker loader

--- docker_builds/rpi_xbuild/dev/assets/loader.sh
+++ docker_builds/rpi_xbuild/dev/assets/loader.sh
@@ -1,13 +1,12 @@
 #! /bin/bash
 
 
-echo -e "\n>> unpacking bundle"
-tar -xvzf BUNDLE.tar.gz --warning=none
-
 echo -e "\n>> loading docker images"
 docker load -i images_rpi.tar
 
 echo -e "\n>> checking out exred source"
-git clone git@github.com:exredorg/exred.git /src
+cd ~
+mkdir src
+git clone https://github.com/exredorg/exred.git src
 
 echo -e "\n>> done"
